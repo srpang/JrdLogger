e75b445b959713b4b62656135763a24e3b86b13f
yuguan.chen <yuguan_chen@icloud.com>
2015-05-21 21:36:21 +0800

    update play-stop button, add action bar which is corresponding to sliding setting panel

--- src/com/jrdcom/jrdlogger/MainActivity.java
+++ src/com/jrdcom/jrdlogger/MainActivity.java
@@ -4,18 +4,27 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ToggleButton;
 
 import com.jrdcom.jrdlogger.framework.JRDLoggerManager;
+import com.jrdcom.jrdlogger.ui.FloatingActionButton;
 
 public class MainActivity extends ActionBarActivity {
 
 	private static final String TAG = "JrdLogger";
-	private ToggleButton mStartStopToggleButton;
+	private FloatingActionButton mStartStopToggleButton;
+	private Toolbar mToolbar;
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
 	private JRDLoggerManager mManager = null;
 	private ProgressDialog mWaitingDialog = null;
 	
@@ -79,7 +88,35 @@ public class MainActivity extends ActionBarActivity {
 	}
 
 	private void findViews() {
-		this.mStartStopToggleButton = ((ToggleButton) findViewById(R.id.startStopToggleButton));
+		this.mStartStopToggleButton = ((FloatingActionButton) findViewById(R.id.startStopToggleButton));
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("JrdLogger");//
+        setSupportActionBar(mToolbar);
+//        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.action_settings:
+//                        Log.v("cyg", "menuItem action_setting");
+//                        Toast.makeText(MainActivity.this, "action_settingsxxx", Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case R.id.home:
+//                        Log.v("cyg", "home");
+//                        break;
+//                    case R.id.homeAsUp:
+//                        Log.v("cyg", "homeAsUp");
+//                    default:
+//                        break;
+//                }
+//                return false;
+//            }
+//        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open,
+                R.string.drawer_close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 	}
 
 	private void initViews() {
@@ -118,4 +155,21 @@ public class MainActivity extends ActionBarActivity {
 		}
 		mMessageHandler.sendMessageDelayed(mMessageHandler.obtainMessage(MSG_WAITING_LOGGING_FINISHED), 1000);
 	}
+	
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                if (mDrawerLayout.isDrawerOpen(Gravity.START)) {
+                    mDrawerLayout.closeDrawer(Gravity.START);
+                } else {
+                    mDrawerLayout.openDrawer(Gravity.START);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
